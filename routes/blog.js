caecd516d7073ae6213687be7dbde9caf702a73b
naresh-khatri <naresh.khatri2345@gmail.com>
2022-04-03 16:51:12 +0530

    Add BlogModel controller functions to /blogs route

--- routes/blog.js
+++ routes/blog.js
@@ -1,26 +1,20 @@
 import express from "express";
-import BlogModel from "../models/BlogModel.js";
+import { checkIfAuthenticated } from "../middlewares/auth-middleware.js";
+import fileUpload from "express-fileupload";
+
+import * as blogController from "../controllers/blogs.js";
 
 const Router = express.Router();
+Router.use(fileUpload());
 
-Router.get("/", async (req, res) => {
-  try {
-    const allBlogs = await BlogModel.find({}).sort({ createdAt: -1 });
-    console.log('allBlogs', allBlogs);
-    res.send(allBlogs);
-  } catch (err) {
-    console.log(err);
-  }
-});
-Router.post("/", async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const newBlog = await BlogModel({ title, content });
-    newBlog.save();
-    res.json(newBlog);
-  } catch (err) {
-    console.log(err);
-  }
-});
+Router.get("/", blogController.getAllBlogs);
+
+Router.get("/:blogId", blogController.getBlog);
+
+Router.delete("/:blogId", blogController.deleteBlog);
+
+Router.patch("/:blogId", checkIfAuthenticated, blogController.updateBlog);
+
+Router.post("/", [checkIfAuthenticated], blogController.createBlog);
 
 export default Router;
