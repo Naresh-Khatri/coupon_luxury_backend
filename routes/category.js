c984d8dc0d576034dce7cc506ca956196f25c32a
naresh-khatri <naresh.khatri2345@gmail.com>
2022-04-06 21:32:54 +0530

    Add new endpoints for searching with slug, name and autocomplete

--- routes/category.js
+++ routes/category.js
@@ -4,17 +4,27 @@ import fileUpload from "express-fileupload";
 
 import * as categoryController from "../controllers/categories.js";
 
-
-
 const Router = express.Router();
 Router.use(fileUpload());
 
 Router.get("/", categoryController.getAllCategories);
 
-Router.get("/:catagory", categoryController.getCategory);
+Router.get("/:categoryId", categoryController.getCategory);
+
+
+Router.get("/getUsingName/:categoryName", categoryController.getCategoryWithName);
+
+Router.get("/getUsingSlug/:categorySlug", categoryController.getCategoryWithSlug);
+Router.get(
+  "/getAllSubCategories/:categoryId",
+  categoryController.getSubCategories
+);
+Router.post("/getAutoCompleteData/", categoryController.getAutoCompleteData);
+
+Router.delete("/:categoryId", categoryController.deleteCategory);
 
 Router.patch(
-  "/:catagory",
+  "/:categoryId",
   checkIfAuthenticated,
   categoryController.updateCategory
 );
