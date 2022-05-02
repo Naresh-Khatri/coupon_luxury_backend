fab2c04d6d379be8fb4e4f53798d45dc566f6f8d
naresh-khatri <naresh.khatri2345@gmail.com>
2022-05-02 20:55:56 +0530

    protect /stats route

--- routes/stats.js
+++ routes/stats.js
@@ -6,7 +6,7 @@ import offerModel from "../models/offerModel.js";
 
 const Router = express.Router();
 
-Router.get("/", async (req, res) => {
+Router.get("/", checkIfAuthenticated, async (req, res) => {
   const storesCount = await storeModel.count();
   const categoriesCount = await categoryModel.count();
   const offersCount = await offerModel.count();
