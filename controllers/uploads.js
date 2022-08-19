7cda94c9f90b5fb3c958db1d91068386b1f6ccc2
naresh-khatri <naresh.khatri2345@gmail.com>
2022-08-19 23:51:12 +0530

    add a file upload controller

---
+++ controllers/uploads.js
@@ -0,0 +1,42 @@
+import blogModel from "../models/BlogModel.js";
+import storeModel from "../models/storeModel.js";
+
+import imageKit from "../config/imagekitConfig.js";
+import { removeImgFromImageKit } from "../config/imagekitConfig.js";
+
+export async function newUpload(req, res) {
+  try {
+    const result = await imageKit.upload({
+      file: req.files.coverImg.data,
+      fileName: "test",
+      folder: "uploads",
+    });
+    console.log(result);
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function getUploads(req, res) {
+  try {
+    const result = await imageKit.listFiles({
+      path: "uploads",
+      sort: "DESC_CREATED",
+      skip: req.query.skip || 0,
+      limit: req.query.limit || 10,
+    });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function deleteUpload(req, res) {
+  try {
+    const result = await imageKit.deleteFile(req.params.uploadId);
+    console.log(result);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+}
