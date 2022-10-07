60aac773d33f67ce01c6f7f699163549c8cf8b0f
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-07 17:31:02 +0530

    migrate code from mongoose to prisma

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,18 +1,25 @@
-import streamifier from "streamifier";
-import categoryModel from "../models/categoryModel.js";
-import subCategoryModel from "../models/subCategoryModel.js";
-
-import cloudinary from "../config/cloudinaryConfig.js";
 import imageKit from "../config/imagekitConfig.js";
+
+import serializer from "../utils/serializer.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function getPublicCategories(req, res) {
   try {
-    const allCategories = await categoryModel
-      .find({ active: true })
-      .sort({ categoryName: 1 });
-    // allCategories.forEach(async (category) =>
-    //   // category.dealsCount
-    // });
+    const categories = await prisma.category.findMany({
+      where: {
+        active: true,
+      },
+      orderBy: {
+        categoryName: "asc",
+      },
+    });
+    res.send(categories);
+
+    // const allCategories = await categoryModel
+    //   .find({ active: true })
+    // .sort({ categoryName: 1 });
+
     res.send(allCategories);
   } catch (err) {
     console.log(err);
@@ -21,7 +28,13 @@ export async function getPublicCategories(req, res) {
 
 export async function getAllCategories(req, res) {
   try {
-    const allCategories = await categoryModel.find().sort({ categoryName: 1 });
+    const allCategories = await prisma.category.findMany({
+      orderBy: {
+        categoryName: "asc",
+      },
+    });
+
+    // const allCategories = await categoryModel.find().sort({ categoryName: 1 });
 
     res.send(allCategories);
   } catch (err) {
@@ -31,41 +44,81 @@ export async function getAllCategories(req, res) {
 
 export async function getCategory(req, res) {
   try {
-    const category = await categoryModel
-      .findById(req.params.categoryId)
-      .sort({ categoryName: -1 });
+    const category = await prisma.category.findUnique({
+      where: {
+        id: parseInt(req.params.categoryId),
+      },
+    });
     res.send(category);
+    // const category = await categoryModel
+    //   .findById(req.params.categoryId)
+    //   .sort({ categoryName: -1 });
+    // res.send(category);
   } catch (err) {
     console.log(err);
   }
 }
 export async function getCategoryWithName(req, res) {
   try {
-    const category = await categoryModel
-      .findOne({
-        categoryName: { $regex: req.params.categoryName, $options: "i" },
-      })
-      .sort({ categoryName: -1 });
+    const category = await prisma.category.findUnique({
+      where: {
+        //regex is case insensitive
+        categoryName: {
+          contains: req.params.categoryName,
+          mode: "insensitive",
+        },
+      },
+    });
     res.send(category);
+
+    // const category = await categoryModel
+    //   .findOne({
+    //     categoryName: { $regex: req.params.categoryName, $options: "i" },
+    //   })
+    //   .sort({ categoryName: -1 });
+    // res.send(category);
   } catch (err) {
     console.log(err);
   }
 }
 export async function getCategoryWithSlug(req, res) {
   try {
-    const category = await categoryModel
-      .findOne({
+    const category = await prisma.category.findUnique({
+      where: {
         slug: req.params.categorySlug,
-      })
-      .populate({
-        path: "offers",
-        populate: {
-          path: "store",
-          select: "_id image storeName storeURL slug",
+      },
+      include: {
+        offers: {
+          where: {
+            active: true,
+          },
+          include: {
+            store: {
+              select: {
+                storeName: true,
+                image: true,
+                slug: true,
+              },
+            },
+          },
         },
-      })
-      .sort({ categorySlug: -1 });
+      },
+    });
     res.send(category);
+
+    // const category = await categoryModel
+    //   .findOne({
+    //     slug: req.params.categorySlug,
+    //   })
+    //   .populate({
+    //     path: "offers",
+    //     populate: {
+    //       path: "store",
+    //       select: "_id image storeName storeURL slug",
+    //     },
+    //   })
+    //   .sort({ categorySlug: -1 });
+    // res.send(category);
   } catch (err) {
     console.log(err);
   }
@@ -78,43 +131,39 @@ export async function createCategory(req, res) {
     // return res.status(200).json({
     //   message: "Category created successfully",
     // });
-    //check if category already exists
-    const categoryExists = await categoryModel.findOne({
-      slug: req.body.slug,
-    });
-    if (categoryExists) {
-      return res.status(409).send("Category already exists");
-    } else {
-      //create new category
 
-      //upload buffer to cloudinary
-      //upload buffer to imageKit
-      const result = await imageKit.upload({
-        file: req.files.image.data,
-        fileName: req.body.slug,
-        extensions: [
-          {
-            name: "google-auto-tagging",
-            maxTags: 5,
-            minConfidence: 95,
-          },
-        ],
-      });
-      // console.log(result);
-      const newCategory = await categoryModel({
-        ...req.body,
-        uid: req.user.uid,
-        image: result.url,
-      });
-      newCategory.save((err, result) => {
-        if (err) {
-          console.log(err);
-          return res.status(400).send("Invalid Request");
-        }
-        res.json(result);
-      });
-      // res.json("noice");
-    }
+    const categoryExists = await prisma.category.findUnique({
+      where: {
+        slug: req.body.slug,
+      },
+    });
+    if (categoryExists)
+      return res.status(409).json({ message: "Category already exists" });
+
+    const result = await imageKit.upload({
+      file: req.files.image.data,
+      fileName: req.body.slug,
+      extensions: [
+        {
+          name: "google-auto-tagging",
+          maxTags: 5,
+          minConfidence: 95,
+        },
+      ],
+    });
+
+    const data = serializer({
+      ...req.body,
+      uid: req.user.uid,
+      image: result.url,
+    });
+    const newCategory = await prisma.category.create({
+      data,
+    });
+    res.status(200).json({
+      message: "Category created successfully",
+      category: newCategory,
+    });
   } catch (err) {
     console.log(err);
     res.status(400).send("Invalid Request");
@@ -126,7 +175,6 @@ export async function updateCategory(req, res) {
     // console.log(req.params.categoryId, req.body, req.files);
     //check if image is provided
     if (req.files && req.files.image) {
-      //upload buffer to cloudinary
       //upload buffer to imageKit
       const result = await imageKit.upload({
         file: req.files.image.data,
@@ -139,60 +187,78 @@ export async function updateCategory(req, res) {
           },
         ],
       });
-
-      const updatedCategory = await categoryModel.findByIdAndUpdate(
-        req.params.categoryId,
-        { ...req.body, image: result.url },
-        { new: false }
-      );
+      const oldImage = await prisma.category.findUnique({
+        where: {
+          id: parseInt(req.params.categoryId),
+        },
+        select: {
+          image: true,
+        },
+      });
+      const data = serializer({
+        ...req.body,
+        image: result.url,
+      });
+      const updatedCategory = await prisma.category.update({
+        where: {
+          id: parseInt(req.params.categoryId),
+        },
+        data,
+      });
       //now remove the old image from imageKit
       //grab the old image from the db
       const oldImageName =
-        updatedCategory.image.split("/")[
-          updatedCategory.image.split("/").length - 1
-        ];
+        oldImage.image.split("/")[oldImage.image.split("/").length - 1];
       //remove the old image from imageKit
       await removeImgFromImageKit(oldImageName);
       res.json(updatedCategory);
     } else {
-      categoryModel
-        .findByIdAndUpdate(req.params.categoryId, req.body, { new: true })
-        .then((result) => {
-          // console.log(result);
-          res.json(result);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      const data = serializer(req.body);
+      const updatedCategory = await prisma.category.update({
+        where: {
+          id: parseInt(req.params.categoryId),
+        },
+        data,
+      });
+      res.json(updatedCategory);
     }
   } catch (err) {
     console.log(err);
+    res.status(400).send("Invalid Request");
   }
 }
 export async function getAutoCompleteData(req, res) {
   try {
     const { searchText } = req.body;
-    const regex = new RegExp(`${searchText}`);
     //do a case insensitive search
-    const category = await categoryModel
-      .find({
-        categoryName: { $regex: regex, $options: "is" },
-      })
-      .select("categoryName slug -_id");
-    // console.log(category)
-    res.send(category);
+    const categories = await prisma.category.findMany({
+      where: {
+        categoryName: {
+          contains: searchText,
+          mode: "insensitive",
+        },
+      },
+      select: {
+        categoryName: true,
+        slug: true,
+      },
+    });
+    res.json(categories);
   } catch (err) {
     console.log(err);
   }
 }
 export async function getSubCategories(req, res) {
   try {
-    const categories = await subCategoryModel
-      .find({
-        category: req.params.categoryId,
-      })
-      .sort({ categoryName: -1 });
-    res.send(categories);
+    const subCategories = await prisma.subCategory.findMany({
+      where: {
+        categoryId: parseInt(req.params.categoryId),
+      },
+      orderBy: {
+        subCategoryName: "asc",
+      },
+    });
+    res.send(subCategories);
   } catch (err) {
     console.log(err);
   }
@@ -200,10 +266,12 @@ export async function getSubCategories(req, res) {
 
 export async function deleteCategory(req, res) {
   try {
-    const deletedCategory = await categoryModel.findByIdAndRemove(
-      req.params.categoryId
-    );
-
+    console.log("delete category", req.params.categoryId);
+    const deletedCategory = await prisma.category.delete({
+      where: {
+        id: parseInt(req.params.categoryId),
+      },
+    });
     res.json(deletedCategory);
   } catch (err) {
     console.log(err);
