4bce83686c97c4bc14882aaa72b19d23c4b54262
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-07 17:31:09 +0530

    migrate code from mongoose to prisma

--- controllers/subCategories.js
+++ controllers/subCategories.js
@@ -2,13 +2,29 @@ import streamifier from "streamifier";
 import categoryModel from "../models/categoryModel.js";
 import subCategoryModel from "../models/subCategoryModel.js";
 
+import serializer from "../utils/serializer.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 export async function getPublicSubCategories(req, res) {
   try {
-    const allCategories = await subCategoryModel
-      .find({ active: true })
-      .populate("category", "categoryName slug")
-      .sort({ subCategoryName: -1 });
-    res.send(allCategories);
+    const subCategories = await prisma.subCategory.findMany({
+      where: {
+        active: true,
+      },
+      include: {
+        category: {
+          select: {
+            categoryName: true,
+            slug: true,
+          },
+        },
+      },
+      orderBy: {
+        subCategoryName: "asc",
+      },
+    });
+    res.send(subCategories);
   } catch (err) {
     console.log(err);
   }
@@ -16,11 +32,20 @@ export async function getPublicSubCategories(req, res) {
 
 export async function getAllSubCategories(req, res) {
   try {
-    const allCategories = await subCategoryModel
-      .find()
-      .populate("category", "categoryName slug")
-      .sort({ subCategoryName: -1 });
-    res.send(allCategories);
+    const allSubCategories = await prisma.subCategory.findMany({
+      include: {
+        category: {
+          select: {
+            categoryName: true,
+            slug: true,
+          },
+        },
+      },
+      orderBy: {
+        subCategoryName: "asc",
+      },
+    });
+    res.send(allSubCategories);
   } catch (err) {
     console.log(err);
   }
@@ -28,11 +53,14 @@ export async function getAllSubCategories(req, res) {
 
 export async function getSubCategory(req, res) {
   try {
-    // console.log(req.params.subCategoryId);
-    //do a case insensitive search
-    const subCategory = await subCategoryModel
-      .findById(req.params.subCategoryId)
-      .sort({ subCategoryName: -1 });
+    const subCategory = await prisma.subCategory.findUnique({
+      where: {
+        id: parseInt(req.params.subCategoryId),
+      },
+      orderBy: {
+        subCategoryName: "asc",
+      },
+    });
     res.send(subCategory);
   } catch (err) {
     console.log(err);
@@ -41,72 +69,63 @@ export async function getSubCategory(req, res) {
 
 export async function createSubCategory(req, res) {
   try {
-    // console.log(req.body);
-    // return res.status(200).json({
-    //   message: "subCategory created successfully",
-    // });
     //create new subCategory
-    const newsubCategory = await subCategoryModel({
+
+    const data = serializer({
       ...req.body,
-      category: req.body.categoryId,
       uid: req.user.uid,
     });
-    //save subCategory in the category collection
-    const category = await categoryModel.findById(req.body.categoryId);
-    // console.log('found category', category);
-    category.subCategories.push(newsubCategory);
-    category.save((err, savedCategory) => {
-      if (err) {
-        console.log("error saving sub in category", err);
-      }
-      // console.log("saved sub in category", savedCategory);
-      newsubCategory.save((err, result) => {
-        if (err) {
-          console.log(err);
-          if (err.code === 11000) {
-            return res.status(409).json({
-              message: "subCategory already exists",
-            });
-          }
-          return res.status(400).send("Invalid Request");
-        }
-        res.json(result);
-      });
+    const newSubCategory = await prisma.subCategory.create({ data });
+
+    //save subCategory in the category relation
+    await prisma.category.update({
+      where: {
+        id: parseInt(req.body.categoryId),
+      },
+      data: {
+        subCategories: {
+          connect: {
+            id: newSubCategory.id,
+          },
+        },
+      },
     });
+    res.json(newSubCategory);
   } catch (err) {
     console.log(err);
-    res.status(400).send("Invalid Request");
+    if (err.code === "P2002")
+      res.status(409).send("SubCategory already exists");
+    else res.status(400).send("Invalid Request");
   }
 }
 
 export async function updateSubCategory(req, res) {
   try {
-    // console.log(req.params.subCategoryId, req.body, req.files);
     //check if image is provided
-    const updatedsubCategory = await subCategoryModel.findByIdAndUpdate(
-      req.params.subCategoryId,
-      req.body,
-      { new: true }
-    );
+    const data = serializer({
+      ...req.body,
+    });
+    const updatedsubCategory = await prisma.subCategory.update({
+      where: {
+        id: parseInt(req.params.subCategoryId),
+      },
+      data,
+    });
     res.json(updatedsubCategory);
   } catch (err) {
     console.log(err);
-    if (err.code === 11000) {
-      return res.status(409).json({
-        message: "subCategory already exists",
-      });
-    }
     return res.status(400).send("Invalid Request");
   }
 }
 
 export async function deleteSubCategory(req, res) {
   try {
-    const deletedCategory = await subCategoryModel.findByIdAndRemove(
-      req.params.subCategoryId
-    );
-
-    res.json(deletedCategory);
+    const deletedSubCategory = await prisma.subCategory.delete({
+      where: {
+        id: parseInt(req.params.subCategoryId),
+      },
+    });
+    res.json(deletedSubCategory);
   } catch (err) {
     console.log(err);
   }
