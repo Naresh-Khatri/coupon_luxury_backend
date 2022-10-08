93df58fb6068676199a6c3a8a291fefd78cb9fa3
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-08 10:25:37 +0530

    migrate code from mongoose to prisma

--- routes/stats.js
+++ routes/stats.js
@@ -1,16 +1,20 @@
 import express from "express";
 import { checkIfAuthenticated } from "../middlewares/auth-middleware.js";
-import storeModel from "../models/storeModel.js";
-import categoryModel from "../models/categoryModel.js";
-import offerModel from "../models/offerModel.js";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const Router = express.Router();
 
 Router.get("/", checkIfAuthenticated, async (req, res) => {
-  const storesCount = await storeModel.count();
-  const categoriesCount = await categoryModel.count();
-  const dealsCount = await offerModel.count({ offerType: "deal" });
-  const couponsCount = await offerModel.count({ offerType: "coupon" });
+  const storesCount = await prisma.store.count();
+  const categoriesCount = await prisma.category.count();
+  const dealsCount = await prisma.offer.count({
+    where: { active: true, offerType: "deal" },
+  });
+  const couponsCount = await prisma.offer.count({
+    where: { active: true, offerType: "coupon" },
+  });
 
   res.json({
     storesCount,
