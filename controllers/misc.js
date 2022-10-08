561f15107d648a048002d90a6c0f7263f4e6f240
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-08 11:32:42 +0530

    migrate code from mongoose to prisma

--- controllers/misc.js
+++ controllers/misc.js
@@ -1,9 +1,26 @@
-import categoryModel from "../models/categoryModel.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function getSitemap(req, res) {
-  const categories = await categoryModel
-    .find({ active: true })
-    .select("image categoryName slug subcategories description")
-    .populate("subCategories", "subCategoryName slug");
+  const categories = await prisma.category.findMany({
+    where: {
+      active: true,
+    },
+    // select: {
+    //   image: true,
+    //   categoryName: true,
+    //   slug: true,
+    //   subCategories: true,
+    //   description: true,
+    // },
+    include: {
+      subCategories: {
+        select: {
+          subCategoryName: true,
+          slug: true,
+        },
+      },
+    },
+  });
   res.json(categories);
 }
