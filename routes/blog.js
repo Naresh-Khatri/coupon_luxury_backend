93c59ea6b664b24bffef4ef20cb46260023aa2ed
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-24 16:55:12 +0530

    create new route to create blogs using appwrite auth

--- routes/blog.js
+++ routes/blog.js
@@ -9,19 +9,18 @@ Router.use(fileUpload());
 
 Router.get("/", blogController.getPublicBlogs);
 
-Router.get("/all",checkIfAuthenticated, blogController.getAllBlogs);
+Router.get("/all", checkIfAuthenticated, blogController.getAllBlogs);
 
 Router.get("/:blogId", blogController.getBlog);
 
-Router.get(
-  "/getUsingSlug/:blogSlug",
-  blogController.getBlogWithSlug
-);
+Router.get("/getUsingSlug/:blogSlug", blogController.getBlogWithSlug);
 
-Router.delete("/:blogId",checkIfAuthenticated, blogController.deleteBlog);
+Router.delete("/:blogId", checkIfAuthenticated, blogController.deleteBlog);
 
 Router.patch("/:blogId", checkIfAuthenticated, blogController.updateBlog);
 
 Router.post("/", [checkIfAuthenticated], blogController.createBlog);
+
+Router.post('/v2', blogController.createBlogV2);
 
 export default Router;
